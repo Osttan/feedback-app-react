aac3f017902ee750a68b5c23e11e24e73aeb1d6f
Andre Ostan <andre.ostan@gmail.com>
2023-04-17 21:01:59 -0300

    Section 3 (Overall) Fully-Completed

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,8 @@
-function FeedbackItem(item) {
+import { FaTimes } from "react-icons/fa";
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
+
+function FeedbackItem({ item, handleDelete }) {
   //   const [rating, setRating] = useState(7);
   //   const [text, setText] = useState("This is an example of a feedback item");
 
@@ -9,13 +13,24 @@ function FeedbackItem(item) {
   //   };
   // console.log("é esse" + item.rating);
 
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
   return (
-    <div className="card">
-      <div className="num-display">{item.item.rating}</div>
-      <div className="text-display">{item.item.text}</div>
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
       {/* <button onClick={handleClick}>Click</button> */}
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
